beab75aa6eb0df8b08f3523f2fb6514eeeb43364
nuprodRomainF <romain.frucco@nuprod.fr>
2025-02-13 15:57:53 +0100

    modified:   geodis_shipping_integration/views/delivery_carrier.xml 	modified:   nuprod_mrp_jf/models/mrp.py

--- nuprod_mrp_jf/models/mrp.py
+++ nuprod_mrp_jf/models/mrp.py
@@ -13,6 +13,6 @@ class nuprod_mrp(models.Model):
     def button_plan(self):
         
         procurement_group_id = self.procurement_group_id
-        sale_order = self.env['sale.order'].search([('procurement_group_id', '=', procurement_group_id)])
+        sale_order = self.env['sale.order'].search([('procurement_group_id', '=', procurement_group_id.id)])
         _logger.warning(sale_order)
         return super().button_plan()
