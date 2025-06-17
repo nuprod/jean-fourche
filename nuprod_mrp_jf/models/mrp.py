b7882a35067e089c4c38daf596edba907c84591f
y4wee <quentin.jambert@gmail.com>
2025-06-17 16:48:31 +0200

    [FIX] commitment_date attribution with mrp button plan

--- nuprod_mrp_jf/models/mrp.py
+++ nuprod_mrp_jf/models/mrp.py
@@ -7,11 +7,16 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class nuprod_mrp(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     def button_plan(self):
+        res = super().button_plan()
         for production in self:
-            sale_order_id = self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+            sale_order_id = (
+                self.procurement_group_id.mrp_production_ids.move_dest_ids.group_id.sale_id
+            )
             sale_order_id.commitment_date = production.date_finished
-        return super().button_plan()
+
+        return res
