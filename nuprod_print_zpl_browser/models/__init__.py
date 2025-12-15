9f37683b8b17d2fc61e43834cbc9ed58363ac32c
nuprodRomainF <romain.frucco@nuprod.fr>
2025-12-15 10:58:20 +0100

    deleted:    nuprod_print_zpl_browser/__init__.py 	deleted:    nuprod_print_zpl_browser/__manifest__.py 	deleted:    nuprod_print_zpl_browser/__pycache__/__init__.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/controllers/__init__.py 	deleted:    nuprod_print_zpl_browser/controllers/__pycache__/__init__.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/controllers/__pycache__/controllers.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/controllers/controllers.py 	deleted:    nuprod_print_zpl_browser/demo/demo.xml 	deleted:    nuprod_print_zpl_browser/models/__init__.py 	deleted:    nuprod_print_zpl_browser/models/__pycache__/__init__.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/mrp_production.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/mrp_workcenter.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/mrp_workorder.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/product.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/stock_lot.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/__pycache__/stock_quant.cpython-311.pyc 	deleted:    nuprod_print_zpl_browser/models/mrp_production.py 	deleted:    nuprod_print_zpl_browser/models/mrp_workcenter.py 	deleted:    nuprod_print_zpl_browser/models/mrp_workorder.py 	deleted:    nuprod_print_zpl_browser/models/product.py 	deleted:    nuprod_print_zpl_browser/models/stock_lot.py 	deleted:    nuprod_print_zpl_browser/models/stock_quant.py 	deleted:    nuprod_print_zpl_browser/report/nuprod_zpl_lot_template.xml 	deleted:    nuprod_print_zpl_browser/report/nuprod_zpl_package_template.xml 	deleted:    nuprod_print_zpl_browser/report/nuprod_zpl_production_template.xml 	deleted:    nuprod_print_zpl_browser/report/nuprod_zpl_quant_template.xml 	deleted:    nuprod_print_zpl_browser/report/nuprod_zpl_report_view.xml 	deleted:    nuprod_print_zpl_browser/security/ir.model.access.csv 	deleted:    nuprod_print_zpl_browser/static/src/description/icon.png 	deleted:    nuprod_print_zpl_browser/static/src/js/BrowserPrint-3.1.250.min.js 	deleted:    nuprod_print_zpl_browser/static/src/js/BrowserPrint-Zebra-1.1.250.min.js 	deleted:    nuprod_print_zpl_browser/static/src/js/bus_service.js 	deleted:    nuprod_print_zpl_browser/static/src/js/mrp_display_action.js 	deleted:    nuprod_print_zpl_browser/static/src/js/mrp_display_record.js 	deleted:    nuprod_print_zpl_browser/static/src/js/view_button.js 	deleted:    nuprod_print_zpl_browser/static/src/js/xml/mrp_display_record.xml 	deleted:    nuprod_print_zpl_browser/views/mrp_production_view.xml 	deleted:    nuprod_print_zpl_browser/views/mrp_workcenter_view.xml 	deleted:    nuprod_print_zpl_browser/views/product_product.xml 	deleted:    nuprod_print_zpl_browser/views/product_template.xml 	deleted:    nuprod_print_zpl_browser/views/stock_lot_view.xml 	deleted:    nuprod_print_zpl_browser/views/stock_quant_package.xml 	deleted:    nuprod_print_zpl_browser/views/stock_quant_view.xml

--- nuprod_print_zpl_browser/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import stock_quant
-from . import product
-from . import mrp_production
-from . import mrp_workcenter
-from . import mrp_workorder
-from . import stock_lot
